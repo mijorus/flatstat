1766c0e949cacc45ce0b1f699988cd92f626855b
Lorenzo Paderi <lorenzo.mijorus@gmail.com>
2022-04-09 00:52:51 +0200

    added footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer class="footer has-background-white mt-6">
+        <div class="content has-text-centered">
+            <p>
+                <strong>Flatstat</strong> by
+                mijorus. Source code on <a href="https://github.com/mijorus/flatstat">GitHub.</a>
+            </p>
+            <p class="is-size-7 has-text-grey">
+                This project is still WIP. Please forget about missing features and bugs.
+            </p>
+        </div>
+    </footer>
+</template>
