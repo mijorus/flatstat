9fbcc6204bad3af65535d31c87b88c79dd57e05c
Lorenzo Paderi <lorenzo.mijorus@gmail.com>
2022-04-09 23:42:54 +0200

    added vue-promised

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import { Promised } from "vue-promised";
 
 const app = createApp(App)
 
 // mount the router
 app.use(router)
+app.component('Promised', Promised)
 
 app.mount('#app')
