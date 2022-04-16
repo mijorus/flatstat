a768b434590977f6e9348b3f53317657f389d2f7
mijorus <lorenzo.mijorus@gmail.com>
2022-04-16 18:28:44 +0200

    fixed overscoll issues on mobile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar is-fixed-top px-2 is-primary" role="navigation" aria-label="main navigation">
+    <nav class="navbar is-fixed-top is-primary" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
             <router-link class="navbar-item" to="/">
                 <img src="/flatstat-badge-logo.svg" alt="flatstat logo" class="mr-2">
