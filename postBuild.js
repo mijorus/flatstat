e5657f425081395b95083b256c4249c0876fd02f
Lorenzo Paderi <lorenzo.mijorus@gmail.com>
2022-08-24 23:58:49 +0200

    fix sitemap

--- postBuild.js
+++ postBuild.js
@@ -1,7 +1,7 @@
 const download = require('download');
 const fs = require('fs')
 
-const filesToDelete = [`${__dirname}/tmp/search_data.json`]
+const filesToDelete = [`${__dirname}}/dist/sitemap.xml`, `${__dirname}}/dist/sitemap.xml`]
 
 filesToDelete.forEach(element => {
     if (fs.existsSync(element)) {
@@ -9,8 +9,7 @@ filesToDelete.forEach(element => {
     }
 });
 
-
-download('https://flathub-stats-backend.vercel.app/search_data.json', `${__dirname}/tmp`, {})
+download('https://flathub-stats-backend.vercel.app/sitemap.xml', `${__dirname}/dist`, {})
     .then(() => {
         console.log('Download Completed');
     })
